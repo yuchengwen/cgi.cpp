3ee6e1d8f311cb1bc16af712bdc1d781e61b388f
ychwn <ychwn@qq.com>
2013-01-05 21:17:13 +0800

    Init the string class

---
+++ tk_string.cpp
@@ -0,0 +1,268 @@
+﻿#include <cstring>
+#include <cstdio>
+#include "cgi_string.h"
+
+using std::vector;
+
+String::String()
+{
+    _capacity = 16;
+    _data = new char[_capacity];
+    memset(_data, 0, _capacity);
+}
+
+String::String(const String & str)
+{
+    _capacity = str.size() + 1;
+    _data = new char[_capacity];
+    memset(_data, 0, _capacity);
+    strcpy(_data, str.data());
+}
+
+String::String(const char * str, size_t size)
+{
+    if(size && size < strlen(str))
+        _capacity = size + 1;
+    else
+        _capacity = strlen(str) + 1;
+    _data = new char[_capacity];
+    memset(_data, 0, _capacity);
+    strncpy(_data, str, _capacity - 1);
+}
+
+String::String(size_t capacity)
+{
+    _capacity = capacity;
+    _data = new char[_capacity];
+    memset(_data, 0, _capacity);
+}
+
+String::~String()
+{
+    delete [] _data;
+}
+
+String String::number(int value)
+{
+    char buf[16] = {0};
+    sprintf(buf, "%d", value);
+    return buf;
+}
+
+String String::number(float value)
+{
+    char buf[16] = {0};
+    sprintf(buf, "%f", value);
+    return buf;
+}
+
+int String::find(const String & str)
+{
+    char * p = strstr(_data, str.data());
+    if(p == 0)
+        return -1;
+    else
+        return p - _data;
+}
+
+size_t String::contain(const String & str)
+{
+    size_t result = 0;
+    char * p = strstr(_data, str.data());
+    while(p)
+    {
+        result++;
+        p += str.size();
+        p = strstr(p, str.data());
+    }
+    return result;
+}
+
+vector<String> String::split(char delim)
+{
+    vector<String> result;
+    char * sub = strtok(_data, &delim);
+    while(sub != 0)
+    {
+        result.push_back(String(sub));
+        sub = strtok(0, &delim);
+    }
+    return result;
+}
+
+String & String::replace(const String & old_part, const String & new_part)
+{
+    if(find(old_part) == -1)
+        return *this;
+        
+    // 替换后字符串的长度
+    _capacity = strlen(_data) + contain(old_part) * (new_part.size() - old_part.size()) + 1;
+    char * str = new char[_capacity];
+    memset(str, 0, _capacity);
+    
+    char * head = _data;
+    char * p = strstr(head, old_part.data());
+    while(p)
+    {
+        strncat(str, head, p - head);
+        strcat(str, new_part.data());
+        head = p + old_part.size();
+        p = strstr(head, old_part.data());
+    }
+    strcat(str, head);
+    
+    delete [] _data;
+    _data = str;
+    return *this;
+}
+
+String & String::trimmed()
+{
+    char black[4] = {' ', '\t', '\r', '\n'};
+    for(int i = 0; i < sizeof(black); i++)
+    {
+        // 删除头部空白字符
+        while(_data[0] == black[i])
+        {
+            size_t last = strlen(_data) - 1;
+            for(int j = 0; j < last; j++)
+                _data[j] = _data[j + 1];
+            _data[last] = '\0';
+        }
+        // 删除尾部空白字符
+        while(_data[strlen(_data) - 1] == black[i])
+            _data[strlen(_data) - 1] = '\0';
+    }
+    return *this;
+}
+
+String String::substr(size_t index, size_t size)
+{
+    if(index >= strlen(_data))
+        return String();
+    return String(_data + index, size);
+}
+
+bool String::startsWith(const String & str)
+{
+    return (strncmp(_data, str.data(), str.size()) == 0);
+}
+
+bool String::endsWith(const String & str)
+{
+    if(str.size() > strlen(_data))
+        return false;
+    char * p = _data + strlen(_data) - str.size();
+    return (strncmp(p, str.data(), str.size()) == 0);
+}
+
+String String::toUpper()
+{
+    int len = strlen(_data) + 1;
+    char * data = new char[len];
+    memset(data, 0, len);
+    strcpy(data, _data);
+    for(int i = 0; i < len; i++)
+    {
+        if(data[i] >= 'a' && data[i] <= 'z')
+            data[i] -= 32;
+    }
+    String result(data);
+    delete [] data;
+    return result;
+}
+
+String String::toLower()
+{
+    int len = strlen(_data) + 1;
+    char * data = new char[len];
+    memset(data, 0, len);
+    strcpy(data, _data);
+    for(int i = 0; i < len; i++)
+    {
+        if(data[i] >= 'A' && data[i] <= 'Z')
+            data[i] += 32;
+    }
+    String result(data);
+    delete [] data;
+    return result;
+}
+
+String String::operator+(const char * str)
+{
+    size_t len = size() + strlen(str) + 1;
+    char * tmp = new char[len];
+    memset(tmp, 0, len);
+    strcpy(tmp, _data);
+    strcat(tmp, str);
+    String result(tmp);
+    delete [] tmp;
+    return result;
+}
+
+String String::operator+(const String & str)
+{
+    return operator+(str.data());
+}
+
+void String::operator+=(const char * str)
+{
+    int len = _capacity;
+    while(len < size() + strlen(str) + 1)
+        len *= 2;
+    if(len != _capacity)
+    {
+        char * tmp = _data;
+        _capacity = len;
+        _data = new char[_capacity];
+        memset(_data, 0, _capacity);
+        strcpy(_data, tmp);
+        delete [] tmp;
+    }
+    strcat(_data, str);
+}
+
+void String::operator+=(const String & str)
+{
+    operator+=(str.data());
+}
+
+void String::operator=(const char * str)
+{
+    int len = _capacity;
+    while(len < strlen(str) + 1)
+        len *= 2;
+    if(len != _capacity)
+    {
+        delete [] _data;
+        _capacity = len;
+        _data = new char[_capacity];
+    }
+    memset(_data, 0, _capacity);
+    strcpy(_data, str);
+}
+
+void String::operator=(const String & str)
+{
+    return operator=(str.data());
+}
+
+bool String::operator==(const char * str)
+{
+    return (strcmp(_data, str) == 0);
+}
+
+bool String::operator==(const String & str)
+{
+    return operator==(str.data());
+}
+
+bool String::operator!=(const char * str)
+{
+    return (strcmp(_data, str) != 0);
+}
+
+bool String::operator!=(const String & str)
+{
+    return operator==(str.data());
+}
---
+++ tk_string.h
@@ -0,0 +1,83 @@
+﻿#ifndef _CGI_STRING_H_130104_
+#define _CGI_STRING_H_130104_
+
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+
+class String
+{
+public:
+    String();
+    String(const String & str);
+    String(const char * str, size_t size = 0);
+    String(size_t capacity);
+    ~String();
+
+    inline char * data() const
+    {
+        return _data;
+    }
+    inline size_t size() const
+    {
+        return strlen(_data);
+    }
+    inline size_t capacity() const
+    {
+        return _capacity;
+    }
+
+    inline bool empty()
+    {
+        return (strlen(_data) == 0);
+    }
+    // 查找子字符串,失败返回-1
+    int find(const String & str);
+    // 子字符串出现次数
+    size_t contain(const String & str);
+    // 分隔字符串
+    std::vector<String> split(char delim);
+    // 字符串替换
+    String & replace(const String & old_part, const String & new_part);
+    // 删除首尾空白字符
+    String & trimmed();
+    // 子字符串
+    String substr(size_t index, size_t size = 0);
+    // 判断字符串是否以某串开头
+    bool startsWith(const String & str);
+    // 判断字符串是否以某串结尾
+    bool endsWith(const String & str);
+    // 大写
+    String toUpper();
+    // 小写
+    String toLower();
+    
+    inline long toLong(int base = 10) const
+    {
+        return strtol(_data, 0, base);
+    }
+    inline double toDouble() const
+    {
+        return strtod(_data, 0);
+    }
+    
+    static String number(int value);
+    static String number(float value);
+
+    String operator+(const String & str);
+    String operator+(const char * str);
+    void operator=(const String & str);
+    void operator=(const char * str);
+    void operator+=(const String & str);
+    void operator+=(const char * str);
+    bool operator==(const String & str);
+    bool operator==(const char * str);
+    bool operator!=(const String & str);
+    bool operator!=(const char * str);
+
+private:
+    char * _data;
+    size_t _capacity;
+};
+
+#endif
